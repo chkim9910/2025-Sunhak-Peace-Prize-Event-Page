0ddaa15f3dfd09f334ad47ad212ce93792d698af
chohyang kim <chkim9910@naver.com>
2024-12-17 14:35:12 +0900

    feature: laureate1

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,30 +1,58 @@
 $(function () {
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
   let sections1 = gsap.utils.toArray("main .laureate1-sect");
-  let scrollTween1 = gsap.to(sections1, {
-    xPercent: -100 * (sections1.length - 1),
-    ease: "none", // <-- IMPORTANT!
+  let scrollTimeline = gsap.timeline({
     scrollTrigger: {
       trigger: ".laureate1-container",
       pin: true,
-      scrub: 0.1,
-      //snap: directionalSnap(1 / (sections.length - 1)),
-      end: "+=3000",
+      scrub: 1,
+      start: "top top",
+      end: "+=6000",
+      markers: true,
     },
   });
+  // 타임라인
+  scrollTimeline
+    .set(sections1, {
+      opacity: 0,
+    })
+    .to(sections1, { opacity: 1, duration: 0.05 })
+    .to(sections1, {
+      xPercent: -100 * (sections1.length - 1),
+    });
 
   let sections2 = gsap.utils.toArray("main .laureate2-sect");
-  let scrollTween2 = gsap.to(sections2, {
-    xPercent: -100 * (sections2.length - 1),
-    ease: "none", // <-- IMPORTANT!
+  let scrollTimeline2 = gsap.timeline({
     scrollTrigger: {
       trigger: ".laureate2-container",
       pin: true,
-      scrub: 0.1,
-      //snap: directionalSnap(1 / (sections.length - 1)),
-      end: "+=3000",
+      scrub: 1,
+      //   start: "top top",
+      end: "+=4500",
+      markers: true,
     },
   });
+  // 타임라인
+  scrollTimeline2
+    .set(sections2, {
+      opacity: 0,
+    })
+    .to(sections2, { opacity: 1, duration: 0.05 })
+    .to(sections2, {
+      xPercent: -100 * (sections2.length - 1),
+    });
+
+  //   let scrollTween2 = gsap.to(sections2, {
+  //     xPercent: -100 * (sections2.length - 1),
+  //     ease: "none", // <-- IMPORTANT!
+  //     scrollTrigger: {
+  //       trigger: ".laureate2-container",
+  //       pin: true,
+  //       scrub: 0.1,
+  //       //snap: directionalSnap(1 / (sections.length - 1)),
+  //       end: "+=3000",
+  //     },
+  //   });
 
   let sections3 = gsap.utils.toArray("main .laureate3-sect");
   let scrollTween3 = gsap.to(sections3, {
@@ -52,3 +80,16 @@ $(function () {
     },
   });
 });
+
+//   let scrollTween1 = gsap.to(sections1, {
+//     xPercent: -100 * (sections1.length - 1),
+//     ease: "none", // <-- IMPORTANT!
+//     scrollTrigger: {
+//       trigger: ".laureate1-container",
+//       pin: true,
+//       scrub: 0.1,
+//       start: "top top",
+//       //   snap: directionalSnap(1 / (sections.length - 1)),
+//       end: "+=3000",
+//     },
+//   });
