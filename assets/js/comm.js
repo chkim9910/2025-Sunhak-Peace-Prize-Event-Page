21d2f3dba7104af6ffb64da4c51224bf7f69dcdd
chohyang kim <chkim9910@naver.com>
2024-12-17 15:35:23 +0900

    fix: laureat1 section

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,6 +1,11 @@
 $(function () {
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
   let sections1 = gsap.utils.toArray("main .laureate1-sect");
+  let section1_1 = document.querySelector(".laureate1-sect-1");
+  let section1_2 = document.querySelector(".laureate1-sect-2");
+  let section1_3 = document.querySelector(".laureate1-sect-3");
+  let section1_4 = document.querySelector(".laureate1-sect-4");
+  let section1_5 = document.querySelector(".laureate1-sect-5");
   let scrollTimeline = gsap.timeline({
     scrollTrigger: {
       trigger: ".laureate1-container",
@@ -13,13 +18,65 @@ $(function () {
   });
   // 타임라인
   scrollTimeline
-    .set(sections1, {
+    .set(section1_1, {
       opacity: 0,
     })
-    .to(sections1, { opacity: 1, duration: 0.05 })
+    .to(section1_1, { opacity: 1, duration: 0.05 })
+
     .to(sections1, {
       xPercent: -100 * (sections1.length - 1),
     });
+  // section1_2 opacity 전환 추가
+  gsap.fromTo(
+    section1_2,
+    { opacity: 0 }, // 초기 상태
+    {
+      opacity: 1, // 뷰포트 진입 시
+      duration: 2,
+      scrollTrigger: {
+        trigger: section1_2, // section1_2를 트리거로 설정
+        containerAnimation: scrollTimeline, // 수평 스크롤 타임라인과 동기화
+        start: "top center", // 섹션이 뷰포트 중심에 도달할 때 시작
+        end: "center center+=100", // 약간의 여유를 둔 후 종료
+        scrub: true, // 스크롤에 따라 부드럽게 전환
+        // markers: true, // 디버깅용
+      },
+    }
+  );
+  // section1_3 opacity 전환 추가
+  gsap.fromTo(
+    section1_3,
+    { opacity: 0 }, // 초기 상태
+    {
+      opacity: 1, // 뷰포트 진입 시
+      duration: 2,
+      scrollTrigger: {
+        trigger: section1_3, // section1_2를 트리거로 설정
+        containerAnimation: scrollTimeline, // 수평 스크롤 타임라인과 동기화
+        start: "start-=1000 center+=1000", // 섹션이 뷰포트 중심에 도달할 때 시작
+        end: "center center+=200", // 약간의 여유를 둔 후 종료
+        scrub: true, // 스크롤에 따라 부드럽게 전환
+        markers: true, // 디버깅용
+      },
+    }
+  );
+  // section1_5 opacity 전환 추가
+  gsap.fromTo(
+    section1_5,
+    { opacity: 0 }, // 초기 상태
+    {
+      opacity: 1, // 뷰포트 진입 시
+      duration: 2,
+      scrollTrigger: {
+        trigger: section1_5, // section1_2를 트리거로 설정
+        containerAnimation: scrollTimeline, // 수평 스크롤 타임라인과 동기화
+        start: "start-=1000 center+=500", // 섹션이 뷰포트 중심에 도달할 때 시작
+        end: "center center+=200", // 약간의 여유를 둔 후 종료
+        scrub: true, // 스크롤에 따라 부드럽게 전환
+        markers: true, // 디버깅용
+      },
+    }
+  );
 
   let sections2 = gsap.utils.toArray("main .laureate2-sect");
   let scrollTimeline2 = gsap.timeline({
